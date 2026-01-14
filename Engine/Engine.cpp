e1d6b92ace33671fd27a1f2298d32260ffe6f215
kernel <kernel@kernel.de>
2026-01-14 21:44:07 +0100

    Projektdateien hinzufügen.

---
+++ Engine/Engine.cpp
@@ -0,0 +1,10 @@
+// Engine.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Dies ist ein Beispiel für eine Bibliotheksfunktion.
+void fnEngine()
+{
+}
